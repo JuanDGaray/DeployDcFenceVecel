54e3b2860e0ab23d35a7cb5e7da33505db4d2103
JuanAGaray <130544436+JuanAGaray@users.noreply.github.com>
2025-04-23 17:32:40 -0500

    Fixed Home My Metrics

--- static/js/components/pdf_proposal.js
+++ static/js/components/pdf_proposal.js
@@ -20,33 +20,3 @@ function autorResizeAllTextAreas() {
 document.addEventListener("DOMContentLoaded", () => {
     autorResizeAllTextAreas()
 });
-
-function generatePDF() {
-    // Selección del contenedor principal
-    const element = document.getElementById('main-container');
-
-    // Configuración de opciones para html2pdf
-    const opt = {
-        filename: `PS_{{ proposal.project_name }}__{{ proposal.id }}__{{now|date:"m-d-y" }}.pdf`,
-        image: { 
-            type: 'jpeg', 
-            quality: 1
-        },
-        html2canvas: { 
-            scale: 2,
-            useCORS: true,
-            logging: false,
-            letterRendering: true
-        },
-        jsPDF: { 
-            unit: 'mm', 
-            format: 'a4', 
-            orientation: 'portrait',
-            hotfixes: ['px_scaling']
-        },
-        margin: [0, 0, 0, 0]
-    };
-
-    // Generación y descarga del PDF
-    html2pdf().set(opt).from(element).save();
-}
