f9fbc91f212ae89e8228247bc03e1bb3c5f44ece
JuanAGaray <130544436+JuanAGaray@users.noreply.github.com>
2025-04-24 16:55:40 -0500

    Duplicate Budget

--- customer/urls.py
+++ customer/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('get_projects/', views.utils_get.get_projects, name='get_projects'),
     path('get_project_quick_info/<int:project_id>/', views.utils_get.get_projects_quick_info, name='get_project_quick_info'),
     path('get_proposal_quick_info/<int:proposal_id>/', views.utils_get.get_proposal_quick_info, name='get_proposal_quick_info'),
-    path('update_proposal_status/<int:proposal_id>/', views.utils_get.update_proposal_status, name='update_proposal_status'),
+    path('update_proposal_status/<int:proposal_id>/', views.utils_get.update_proposal_status, name='update_proposal_status'),\
+    path('get_array_projects/', views.utils_get.get_array_projects, name='get_array_projects'),
     ]
--- customer/views/utils_get.py
+++ customer/views/utils_get.py
@@ -64,6 +64,18 @@ def get_proposals(request, page=1):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
     
+@login_required
+def get_array_projects(request):
+    projects = list(Project.objects.all().order_by('project_name').values())
+    projects_by_status = {}
+    for project in projects:
+        status = project['status']
+        if status not in projects_by_status:
+            projects_by_status[status] = []
+        projects_by_status[status].append(project)
+    print(projects_by_status)
+    return JsonResponse(projects_by_status, safe=False)
+
 
 @login_required
 def get_projects(request, page=1):
